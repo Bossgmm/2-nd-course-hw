7e2fd89dc57606d2af1a6ef89570524b41ee7c81
Murad <goysultanov.murad@gmail.com>
2025-11-20 16:12:20 +0300

    Выполнение заданий

--- hw8-Tasks.js
+++ hw8-Tasks.js
@@ -1,45 +1,45 @@
-// // Задание 1
-// const people = [
-//    { name: 'Глеб', age: 29 },
-//    { name: 'Анна', age: 17 },
-//    { name: 'Олег', age: 7 },
-//    { name: 'Оксана', age: 47 }
-// ];
+// Задание 1
+const people1 = [
+   { name: 'Глеб', age: 29 },
+   { name: 'Анна', age: 17 },
+   { name: 'Олег', age: 7 },
+   { name: 'Оксана', age: 47 }
+];
 
-// people.sort((a, b) => a.age - b.age);
+people1.sort((a, b) => a.age - b.age);
 
-// console.log(people);
+console.log(people1);
 
-// // Задание 2
-// function isPositive(number) {
-//   // писать код тут
-//   return number > 0;
-// }
-// function isMale(person) {
-//   // писать код тут
-//   return person.gender === "male";
-// }
-// function filter(array, ruleFunction) {
-//   // писать код тут
-//   const result = [];
-//   for (let i = 0; i < array.length; i++) {
-//     if (ruleFunction(array[i])) {
-//       result.push(array[i]);
-//     }
-//   }
-//   return result;
-// }
+// Задание 2
+function isPositive(number) {
+  // писать код тут
+  return number > 0;
+}
+function isMale(person) {
+  // писать код тут
+  return person.gender === "male";
+}
+function filter(array, ruleFunction) {
+  // писать код тут
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (ruleFunction(array[i])) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
 
-// console.log(filter([3, -4, 1, 9], isPositive));
+console.log(filter([3, -4, 1, 9], isPositive));
 
-// const people = [
-//   { name: "Глеб", gender: "male" },
-//   { name: "Анна", gender: "female" },
-//   { name: "Олег", gender: "male" },
-//   { name: "Оксана", gender: "female" },
-// ];
+const people2 = [
+  { name: "Глеб", gender: "male" },
+  { name: "Анна", gender: "female" },
+  { name: "Олег", gender: "male" },
+  { name: "Оксана", gender: "female" },
+];
 
-// console.log(filter(people, isMale));
+console.log(filter(people2, isMale));
 
 // Задание 3
 const printDate = () => {
@@ -54,3 +54,32 @@ const printDate = () => {
 };
 
 printDate();
+
+// Задание 4
+function delayForSecond(callback) {
+    setTimeout(callback, 1000);
+}
+
+delayForSecond(function () {
+   console.log('Привет, Глеб!');
+})
+
+// Задание 5
+// Функция delayForSecond через 1 секунду пишет в консоль 
+// «Прошла одна секунда», а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
+
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi (name) {
+    console.log('Привет, ${name}!');
+}
+
+// Код выше менять нельзя
+
+// Нужно изменить код ниже:
+delayForSecond(() => sayHi('Глеб'));
